276c30dea519838c7e9b78ba6cec30aeba3cf2ee
Marcos Trejo <marcostrejo23@gmail.com>
2021-07-20 20:25:02 -0700

    pushing engineer extend

--- 02-Homework/Develop/lib/Engineer.js
+++ 02-Homework/Develop/lib/Engineer.js
@@ -1,24 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-inquirer.prompt(
-    [
-        {
-            type: `input`,
-            message: `what is your name?`,
-            name: `name`,
-        },
-        {
-            type: `input`,
-            message: `what is your id?`,
-            name: `id`,
-        },
-        {
-            type: `input`,
-            message: `what is your email?`,
-            name: `email`,
-        },
-        {
-            type: `input`,
-            message: `what is your github username?`,
-            name: `github`,
-        },
-    ]);
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+    constructor(name, id, email, github){
+        super(name, id, email);
+        this.github = github;
+    }
+    getGithub(){
+        return this.github;
+    }
+    getRole(){
+        return Engineer.name;
+    }
+}
+
+module.exports = Engineer;

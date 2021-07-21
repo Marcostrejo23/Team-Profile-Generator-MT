57fc46b6e7eb2731c5b68326111ccdc7305234c3
Marcos Trejo <marcostrejo23@gmail.com>
2021-07-20 20:11:47 -0700

    pushing employee class

--- 02-Homework/Develop/lib/Engineer.js
+++ 02-Homework/Develop/lib/Engineer.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+inquirer.prompt(
+    [
+        {
+            type: `input`,
+            message: `what is your name?`,
+            name: `name`,
+        },
+        {
+            type: `input`,
+            message: `what is your id?`,
+            name: `id`,
+        },
+        {
+            type: `input`,
+            message: `what is your email?`,
+            name: `email`,
+        },
+        {
+            type: `input`,
+            message: `what is your github username?`,
+            name: `github`,
+        },
+    ]);

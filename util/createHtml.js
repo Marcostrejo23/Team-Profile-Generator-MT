ced4dc9de1e31f14405f41d62d1b5c7204707b8f
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-29 13:06:32 -0700

    create team

--- util/createHtml.js
+++ util/createHtml.js
@@ -59,3 +59,38 @@ const createTeam = team =>{
     html.push(team.filter(employee=>employee.getRole()==intern)).map(intern =>createIntern(intern));
     return html.join("");
 };
+
+const createHtml = (team)=>{
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    <body class="bg-danger">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading bg-primary">
+                    <h1 class="text-center text-white">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex flex-wrap justify-content-center">
+                    ${createTeam(team)}
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+        `;
+};
+
+module.exports = createHtml

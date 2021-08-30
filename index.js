97492af1473d5dbae1fefa8b07233fbb5bd6dd66
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-29 17:24:37 -0700

    nswitch case for continuing

--- index.js
+++ index.js
@@ -8,107 +8,107 @@ const Intern = require("./lib/Intern")
 let team = [];
 
 
-function createMember(){
-    return inquirer.prompt([
-        {
-        type: "input",
-        message: "what is the member's name?",
-        name: "name",
-        validate: function(data){
-            if(data.length <= 0){
-                return "You must input a name"
-            }
-            return true
-        }
-    },
-    {
-        type: "list",
-        message: "what is the member's position?",
-        choices: ["manager","engineer","intern"],
-        name: "position"
-    },
-    {
-        type: "input",
-        message: "what is the member's email",
-        name: "email",
-        validate: function(data){
-            if(data.includes(`@`) === false){
-                return "Need a valid email"
-            }
-            return true
-        }
-    },
-    {
-        type: "input",
-        message: "What is the member's id?",
-        name: "id",
-        validate: function(data){
-            if (isNaN(parseInt(data))){
-                return "An Id must be a number"
-            }
-            return true
-        }
-    }
-    ]).then(function({name,position,email,id}){
-        let positionData = "";
-        if (position === "engineer"){
-            positionData = "GitHub username";
-        }else if (position === "intern") {
-            positionData = "school name";
-        }else {
-            positionData = "office number";
-        }
-        inquirer.prompt([
-           {
-            type: "input",
-            message: `Enter the member's ${positionData}`,
-            name: "positionData",
-            validate: function(data){
-                if(role==="manager"){
-                    if(isNaN(parseInt(data))){
-                        return "a number is required"
-                    }
-                    return true 
-                }
-                return true 
-            }
-        },
-        {
-            type: "confirm",
-            message: "Are there more members to introduce?",
-            name: "more"
-        }
-        ]).then(function({positionData, more}){
-            let teamMember;
-            if(role ==="manager"){
-                teamMember= new Manager(name, id, email, positionData)
-            }else if(role==="engineer"){
-                teamMember= new Engineer(name,id,email,positionData)
-            }else{
-                teamMember = new Intern(name,id,email,positionData)
-            }
-            team.push(teamMember)
-            if(more===true){
-                createMember()
-            }else{
-                let str = createHtml(team)
-                writeHtml(str)
-            }
-        })
-    })
-}
+// function createMember(){
+//     return inquirer.prompt([
+//         {
+//         type: "input",
+//         message: "what is the member's name?",
+//         name: "name",
+//         validate: function(data){
+//             if(data.length <= 0){
+//                 return "You must input a name"
+//             }
+//             return true
+//         }
+//     },
+//     {
+//         type: "list",
+//         message: "what is the member's position?",
+//         choices: ["manager","engineer","intern"],
+//         name: "position"
+//     },
+//     {
+//         type: "input",
+//         message: "what is the member's email",
+//         name: "email",
+//         validate: function(data){
+//             if(data.includes(`@`) === false){
+//                 return "Need a valid email"
+//             }
+//             return true
+//         }
+//     },
+//     {
+//         type: "input",
+//         message: "What is the member's id?",
+//         name: "id",
+//         validate: function(data){
+//             if (isNaN(parseInt(data))){
+//                 return "An Id must be a number"
+//             }
+//             return true
+//         }
+//     }
+//     ]).then(function({name,position,email,id}){
+//         let positionData = "";
+//         if (position === "engineer"){
+//             positionData = "GitHub username";
+//         }else if (position === "intern") {
+//             positionData = "school name";
+//         }else {
+//             positionData = "office number";
+//         }
+//         inquirer.prompt([
+//            {
+//             type: "input",
+//             message: `Enter the member's ${positionData}`,
+//             name: "positionData",
+//             validate: function(data){
+//                 if(role==="manager"){
+//                     if(isNaN(parseInt(data))){
+//                         return "a number is required"
+//                     }
+//                     return true 
+//                 }
+//                 return true 
+//             }
+//         },
+//         {
+//             type: "confirm",
+//             message: "Are there more members to introduce?",
+//             name: "more"
+//         }
+//         ]).then(function({positionData, more}){
+//             let teamMember;
+//             if(role ==="manager"){
+//                 teamMember= new Manager(name, id, email, positionData)
+//             }else if(role==="engineer"){
+//                 teamMember= new Engineer(name,id,email,positionData)
+//             }else{
+//                 teamMember = new Intern(name,id,email,positionData)
+//             }
+//             team.push(teamMember)
+//             if(more===true){
+//                 createMember()
+//             }else{
+//                 let str = createHtml(team)
+//                 writeHtml(str)
+//             }
+//         })
+//     })
+// }
 
-function writeHtml(str){
-    fs.writeFile("./output/index.html", str, err=>{
-        if(err) throw err
-        console.log("Generated HTML File")
-    })
-}
-function init(){
-    createMember()
-}
+// function writeHtml(str){
+//     fs.writeFile("./output/index.html", str, err=>{
+//         if(err) throw err
+//         console.log("Generated HTML File")
+//     })
+// }
+// function init(){
+//     createMember()
+// }
 
-init()
+// init()
 
 
 const managerQ = () =>{
@@ -148,11 +148,43 @@ const managerQ = () =>{
         name: "officeNum",
         validate: officeNum => {
             if(isNaN(officeNum)) {
-                return "Please enter a number";
+                return "That is not a number!";
             } else {
                 return true;
             }
         }
     }
-    
-    ]);
+    ]).then(answer =>{
+        const {managerId, managerName,managerEmail,officeNum} =answer;
+        const manager = new Manager{managerId, managerName,managerEmail,officeNum}
+        team.push(manager);
+    })
+};
+
+const continueQ= () =>{
+   return inquirer.prompt([
+    {
+        type: "list",
+        message: "Which member would you like to add?",
+        name: "PositionInfo",
+        choices: ["Engineer", "Intern", "I don't want to add any more members at this time."]
+    }
+   ]).then(answer =>{
+       switch (answer.positionInfo){
+           case "Engineer":
+           engineerQ();
+           break;
+
+           case "Intern":
+           internQ();
+           break;
+
+           case "I don't want to add any more members at this time.":
+               let data = createHtml(team);
+               break;
+       }
+   })
+
+}
+
+const engineerQ
